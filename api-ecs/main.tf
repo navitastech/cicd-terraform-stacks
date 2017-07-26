e9698231c5ee3e9c5f5cb5b0fe6f8e410f484459
Sudheer Dandamudi <bala@JinnyMac.local>
2017-07-26 01:45:25 -0400

    Updating

--- api-ecs/main.tf
+++ api-ecs/main.tf
@@ -17,7 +17,7 @@ resource "aws_launch_configuration" "launch_config" {
   iam_instance_profile = "ecs-instance-role"
   instance_type = "t2.large"
   security_groups = ["sg-129ead63"]
-  user_data = "${coalesce(var.ec2_user_data_raw, file(var.ec2_user_data))}"
+  user_data = "${template_file.user_data.rendered}"
   lifecycle {
     create_before_destroy = true
   }
@@ -57,4 +57,59 @@ resource "aws_autoscaling_group" "main_asg" {
     create_before_destroy = true
   }
 }
+
+
+resource "template_file" "user_data" {
+	template = "${file("${path.module}/userdata.sh")}"
+
+  vars {
+    ecs_cluster_name = "${var.ecs_cluster_name}"
+    app_env = "${var.app_env}"
+
+  }
+}
+
+
+resource "aws_ecs_service" "demo_api_service" {
+    name = "demo_api_service"
+    cluster = "${aws_ecs_cluster.ecscluster.id}"
+    task_definition = "${aws_ecs_task_definition.demo_api_task.arn}"
+    desired_count = 1
+    iam_role = "arn:aws:iam::550522744793:role/ecsServiceRole"
+    
+    load_balancer {
+        elb_name = "${module.demo_service_elb.elb_id}"
+        container_name = "demo-api-service"
+        container_port = 8080
+    }
+}
+
+
+resource "aws_ecs_task_definition" "demo_api_task" {
+    family = "demo_api_service"
+    container_definitions = "${file("task-definitions/demo-api-task.json")}"
+
+
+}
+
+
+module "demo_service_elb" {
+  source = "../modules/tf_module_elb"
+  elb_name="demo-service-${var.app_env}-elb"
+  subnets = "subnet-11b9213d"
+  security_groups = "sg-85240af4"
+  elb_healthcheck_url = "HTTP:8080/index.html"
+  instance_port = 8080
+  internal_elb = "false"
+  unhealthy_threshold = "10"
+  instance_protocol = "http"
+  app_name = "demo_service"
+  app_env = "${var.app_env}"
+  stack_name="${var.stack_name}"
+  aws_region = "${var.aws_region}"
+  }
+
+
+
+
 
--- api-ecs/userdata.sh
+++ api-ecs/userdata.sh
@@ -1,3 +1,3 @@
+#!/bin/bash
 
- 
- echo "ECS_CLUSTER=${ecs_cluster_name}_${app_env}" >> /etc/ecs/ecs.config
+echo ECS_CLUSTER=${ecs_cluster_name}_${app_env} >> /etc/ecs/ecs.config
